c5df65a7d8a6f2addd82920781414e4ed1b8c0bb
Steven Chen Hao Nyeo <limpingstone@Lstone529-PCA.student.cwru.edu>
2019-03-15 14:48:57 -0400

    Console.java: open method completed and verified

--- src/main/java/app/Console.java
+++ src/main/java/app/Console.java
@@ -1,6 +1,23 @@
-public class Console {
-    public void open() {
+import java.io.IOException;
+import java.awt.Desktop;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+public class Console {
+
+    // Source: https://stackoverflow.com/questions/5226212/how-to-open-the-default-webbrowser-using-java
+    public void open() {
+        String url = "http://localhost:420";
+
+        try {
+            if (Desktop.isDesktopSupported())
+                Desktop.getDesktop().browse(new URI(url));
+            else
+                Runtime.getRuntime().exec("xdg-open " + url);
+        }
+        catch (IOException | URISyntaxException e) {
+            System.out.println("Cannot open browser with url: " + url);
+        }
     }
 
     public void startSetup() {
