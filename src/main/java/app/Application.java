8a9f34b0ac28c1e1c654685c1321ae0244463e1d
Kris Zhao <kriszhao@comcast.net>
2019-03-14 22:18:33 -0400

    address testing

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -14,6 +14,11 @@ public class Application {
         return "Hello Docker World";
     }
 
+    @RequestMapping("/control")
+    public String control() {
+        return "<h1>control</h1>\n<p>This is <em>in</em> a paragraph</p>\n<p>With a link to <a href=\"https://www.youtube.com\">youtube</a></p>";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
