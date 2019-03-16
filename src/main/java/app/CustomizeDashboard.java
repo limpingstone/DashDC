2285d574e0aec931f0a7711c970e654745a9712f
Steven Chen Hao Nyeo <limpingstone@Lstone529-PCA.student.cwru.edu>
2019-03-15 23:01:34 -0400

    CustomizeDashboard.java: added methods

--- src/main/java/app/CustomizeDashboard.java
+++ src/main/java/app/CustomizeDashboard.java
@@ -1,5 +1,143 @@
 package app;
 
 public class CustomizeDashboard {
+    public void signalCreatePage() {
 
+    }
+
+    public void signalCreatePage(int order) {
+
+    }
+
+    public void signalCreateTile(int id, int posX, int posY, int sizeX, int sizeY) {
+
+    }
+
+    public void signalCreateAssetImage(int id, String name, String path, String pathType, int posX, int posY, int sizeX, int sizeY) {
+
+    }
+
+    public void signalCreateAssetLink(int id, String name, String linkPath, int posX, int posY, int sizeX, int sizeY) {
+
+    }
+
+    public void signalCreateAssetList(int id, String name, int posX, int posY, int sizeX, int sizeY) {
+
+    }
+
+    public void signalCreateAssetNotes(int id, String name, int posX, int posY, int sizeX, int sizeY) {
+
+    }
+
+    public void signalCreateAssetCanvas(int id, String name, int posX, int posY, int sizeX, int sizeY) {
+
+    }
+
+    public void signalCreateAssetWeather(int id, String name, int posX, int posY, int sizeX, int sizeY) {
+
+    }
+
+    public void signalCreateAssetDocs(int id, String name, int posX, int posY, int sizeX, int sizeY) {
+
+    }
+
+    public void signalCreateAssetCalendar(int id, String name, int posX, int posY, int sizeX, int sizeY) {
+
+    }
+
+    public void signalCreateAssetHTML(int id, String name, int posX, int posY, int sizeX, int sizeY, String htmlCode) {
+
+    }
+
+    public void signalDuplicatePage(int toBeDuplicated) {
+
+    }
+
+    public void signalDuplicatePage(int toBeDuplicated, int pageIndex) {
+
+    }
+
+    public void signalDuplicateTile(int toBeDuplicated, int id, int posX, int posY) {
+
+    }
+
+    public void signalDuplicateAsset(int toBeDuplicated, int id, int posX, int posY) {
+
+    }
+
+    public void signalDeletePage(int toBeDeleted) {
+
+    }
+
+    public void signalDeleteTile(int toBeDeleted) {
+
+    }
+
+    public void signalDeleteAsset(int toBeDeleted) {
+
+    }
+
+    public void orderPage(int pageInserted, int index) {
+
+    }
+
+    public void editPageColor(int id, int color) {
+
+    }
+
+    public void editPageColor(int id, int color) {
+
+    }
+
+    public void editTileSize(int id, int sizeX, int sizeY) {
+
+    }
+
+    public void editTileColor(int id, int color) {
+
+    }
+
+    public void editTileFont(int id, int font) {
+
+    }
+
+    public void editTilePosition(int id, int posX, int posY) {
+
+    }
+
+    public void editAssetSize(int id, int sizeX, int sizeY) {
+
+    }
+
+    public void editAssetFont(int id, int font) {
+
+    }
+
+    public void editAssetPosition(int id, int posX, int posY) {
+
+    }
+
+    public void editAssetLink(int id, String link) {
+
+    }
+
+    public void editUndo() {
+
+    }
+
+    public void editRedo() {
+
+    }
+
+    public void signalAutosave() {
+
+    }
+
+    public void signalSave() {
+
+    }
+
+    public void signalClose() {
+
+    }
 }
