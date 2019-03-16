373906ccef9fd574fc531a2fb314fdb2868d5521
Kris Zhao <kriszhao@comcast.net>
2019-03-15 21:03:39 -0400

    Restored application

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -12,16 +12,7 @@ public class Application {
     /*  Determining addresses for springboot    */
     @RequestMapping("/")
     public String dash() {
-
-        /*  Testing images  */
-        String agregate = "<h1>User Dashboard</h1>";
-        int[] assetSize = {400,400};
-        int[] assetPosition = new int[2];
-        AssetImage cat = new AssetImage("https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg", assetSize, assetPosition);
-        agregate += cat.display();
-        /*  End image test*/
-
-        return agregate;
+        return "<h1>User Dashboard</h1>";
     }
 
     @RequestMapping("/control-panel")
